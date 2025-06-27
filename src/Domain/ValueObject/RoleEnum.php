ee8edbf6880d82d1416c9b0545014ff6bcc97d2e
Igor Ekimow <igorekimow@gmail.com>
2025-06-27 15:52:03 +0300

    Role setup is done

--- src/Domain/ValueObject/RoleEnum.php
+++ src/Domain/ValueObject/RoleEnum.php
@@ -5,5 +5,7 @@ namespace App\Domain\ValueObject;
 enum RoleEnum: string
 {
     case ROLE_ADMIN = 'ROLE_ADMIN';
+    case ROLE_STUDENT = 'ROLE_STUDENT';
+    case ROLE_TEACHER = 'ROLE_TEACHER';
     case ROLE_USER = 'ROLE_USER';
 }
